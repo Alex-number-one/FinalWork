a53f1dc70914e828242f2cf71a7a1b07f7508be1
Alex <aleksejtatukov098@gmail.com>
2023-07-25 22:25:07 +0300

    task64

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Write a number");
+ int N = Convert.ToInt32(Console.ReadLine());
+int function(int N)
+{
+
+    if (N == 1) return 1;
+    Console.Write(N + ", ");
+    return function(N - 1);
+    
+}
+Console.WriteLine(function(N));
