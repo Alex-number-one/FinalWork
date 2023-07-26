f1b329543343f3fa2747d7fdf8421a06e736bbb5
Alex <aleksejtatukov098@gmail.com>
2023-07-26 11:55:57 +0300

    task68

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int A(int i, int j)
+{
+ if (i == 0) return j + 1;
+ else if (j == 0) return A(i - 1, 1);
+ else
+ {
+return A(i - 1, A(i, j - 1));
+ }
+        
+    
+}
+Console.WriteLine(A(3, 2));
